b2777fcc5fbc21a5e984613354c9f13d60ae2c7a
Roxane RAFFI KHANSARI <rraffi-k@bess-f2r1s4.clusters.42paris.fr>
2024-03-02 15:29:21 +0100

    feat: check_map_is_closed

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 18:06:05 by rraffi-k          #+#    #+#             */
-/*   Updated: 2024/03/01 17:48:38 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2024/03/02 15:27:51 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,45 @@ int get_map_nb_lines(char **map)
 	return (i);
 }
 
-int check_map_first_line(char **map)
+int	get_map_width(char **map)
+{
+	int	i;
+	int	j;
+	int height;
+	int	width;
+
+	i = 0;
+	width = 0;
+	height = get_map_nb_lines(map);
+	while (i < height)
+	{
+		j = 0;
+		while (map[i][j])
+			j++;
+		if (j > width)
+			width = j;
+		i++;
+	}
+	return (width);
+}
+
+int check_upper_wall(char **map)
 {
 	int i;
 	int j;
 
 	j = 0;
-	int len = get_map_nb_lines(map);
+	int height = get_map_nb_lines(map);
 	while (map[0][j])
 	{
 		if (map[0][j] != '1' && map[0][j] != ' ')
 			return (-1);
 
 		i = 0;
-		while (i < len && map[i][j] && map[i][j] == ' ')
+		while (i < height && map[i][j] && map[i][j] == ' ')
 		{				
 			++i;
-			if (i < len)
+			if (i < height)
 			{
 				if (map[i][j] 
 					&& (map[i][j] != ' ' && map[i][j] != '1')
@@ -63,7 +85,7 @@ int check_map_first_line(char **map)
 	return (EXIT_SUCCESS);
 }
 
-int check_map_last_line(char **map)
+int check_bottom_wall(char **map)
 {
 	int i;
 	int j;
@@ -73,15 +95,17 @@ int check_map_last_line(char **map)
 	j = 0;
 	while (map[end][j])
 	{
-		if (map[end][j] != '1' && map[end][j] != ' ')
+		i = end;
+		
+		if (map[i][j] != '1' && map[i][j] != ' ')
 			return (-1);
 
-		i = end;
 		while (i >= 0 && map[i][j] && map[i][j] == ' ')
 		{				
 			--i;
 			if (i >= 0)
 			{
+				//CHECK SEGFAULT DANS LE CAS OU MAP[I][J - 1] OU MAP[I][J + 1] N'EXISTENT PAS
 				if (map[i][j] 
 					&& (map[i][j] != ' ' && map[i][j] != '1')
 					|| ((map[i][j] && map[i][j] != '1' && map[i][j + 1] && map[i][j + 1] != '1' && map[i][j + 1] != ' ')
@@ -95,23 +119,121 @@ int check_map_last_line(char **map)
 	return (EXIT_SUCCESS);
 }
 
+int check_left_wall(char **map)
+{
+	int i;
+	int j;
+	int	height;
 
-//check les murs exterieurs gauche et droite :
-// faire while(espace) pour passer les espaces, puis verifier que c'est un 1 ou que 
-// toute la ligne est vide
+	i = 0;
+	j = 0;
+	height = get_map_nb_lines(map);
+	while (i < height && map[i][j])
+	{
+		j = 0;
+		
+		if (map[i][j] != '1' && map[i][j] != ' ')
+			return (-1);
+
+		while (i < height && map[i][j] && map[i][j] == ' ')
+		{				
+			++j;
+			if (map[i][j])
+			{
+				if ((map[i][j] != ' ' && map[i][j] != '1')
+					|| ((map[i][j] && map[i][j] != '1' && i > 0 && map[i - 1][j] && map[i - 1][j] != '1' && map[i - 1][j] != ' ')
+					|| (map[i][j] && map[i][j] != '1' && i < height - 1 && map[i + 1][j] && map[i + 1][j] != '1' && map[i + 1][j] != ' ')))
+						return (-1);
+			}
+
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int check_right_wall(char **map)
+{
+	int i;
+	int j;
+	int	height;
+	int	width;
+
+	i = 0;
+	width = get_map_width(map);
+	j = width - 1;
+	height = get_map_nb_lines(map);
+	
+	while (i < height && map[i][j])
+	{
+		j = width - 1;
+		
+		if (map[i][j] != '1' && map[i][j] != ' ')
+			return (-1);
+
+		while (i < height && map[i][j] && map[i][j] == ' ')
+		{				
+			--j;
+			if (map[i][j])
+			{
+				if ((map[i][j] != ' ' && map[i][j] != '1')
+					|| ((map[i][j] && map[i][j] != '1' && i > 0 && map[i - 1][j] && map[i - 1][j] != '1' && map[i - 1][j] != ' ')
+					|| (map[i][j] && map[i][j] != '1' && i < height - 1 && map[i + 1][j] && map[i + 1][j] != '1' && map[i + 1][j] != ' ')))
+						return (-1);
+			}
+
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	check_map_is_closed(char **map)
+{
+	int i1 = check_bottom_wall(map) ;
+	int i2 = check_upper_wall(map);
+	int i3 = check_left_wall(map);
+	int i4 = check_right_wall(map);
+
+	printf("%d\n%d\n%d\n%d\n", i1, i2, i3, i4);
+
+	if (check_bottom_wall(map) == -1
+		|| check_upper_wall(map) == - 1
+		|| check_left_wall(map) == -1
+		|| check_right_wall(map) == -1)
+		return (-1);
+	return (EXIT_SUCCESS);
+}
+
+
 
 int main()
 {
 	char **map;
 
-	map = (char**)malloc(4 * sizeof(char*));
+	map = (char**)malloc(14 * sizeof(char*));
 
     // Assigning values to each element in the array
-    map[0] = " 111110111";
-    map[1] = " 101  1001";
-    map[2] = " 111  1111";
+    map[0] =  "        1111111111111111111111111";
+    map[1] =  "        1000000000110000000000001";
+    map[2] =  "    11111011000001110000000000001";
+    map[3] =  "        1001000000000000000000001";
+    map[4] =  "111111111011000001110000000000001";
+    map[5] =  "100000000011000001110111111111111";
+    map[6] =  "10110111111111011100000010001    ";
+    map[7] =  "10110111111111011101010010001    ";
+    map[8] =  "11000000110111011100000010001    ";
+    map[9] =  "100000000000   01100000010001    ";
+    map[10] = "10000000000000001101010010001    ";
+	map[11] = "1100000111010101111101111000111  ";
+    map[12] = "11110111 1110101 101111010001    ";
+    map[13] = "11111111 1111111 111111111111    ";
 
-	printf("%d\n", check_map_last_line(map));
+
+
+
+	// printf("yo\n");
+	printf("RESULTAT = %d\n", check_map_is_closed(map));
 
 	return (0);
 }
